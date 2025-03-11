dd50d4548f2a55b834a7dc9bdf080ca32891f49a
Nicholas Dwenger <dwengern@uindy.edu>
2025-03-11 11:50:25 -0400

    Deployed

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Portfolio App',
-        imgUrl: '/assets/portfolio-app.jpg',
+        imgUrl: 'assets/portfolio-app.jpg',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://github.com/dwengern',
     },
     {
         title: 'Chemistry Inventory Management App',
-        imgUrl: '/assets/chem-project.jpg',
+        imgUrl: 'assets/chem-project.jpg',
         stack: ['Django', 'HTML', 'Javascript', 'SQL'],
         link: 'https://github.com/dwengern',
     },
     {
         title: 'Employee Client Management System',
-        imgUrl: '/assets/emp-client-project.jpg',
+        imgUrl: 'assets/emp-client-project.jpg',
         stack: ['C#', 'Angular', 'ASP.NET', 'Typescript'],
         link: 'https://github.com/dwengern',
     },
